980447668f003b68e37aceb6abed6b4cf6c8b377
Kaan Arslan <115875320+kaan-ders@users.noreply.github.com>
2023-03-11 14:14:01 +0300

    init

---
+++ Movie.cs
@@ -0,0 +1,26 @@
+﻿namespace APIConsume
+{
+    public class MovieList
+    {
+        public int page { get; set; }
+        public List<Movie> results { get; set; } = new();
+    }
+
+    public class Movie
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string overview { get; set; }
+        public string poster_path { get; set; }
+
+        public List<int> genre_ids { get; set; }
+
+        public string Poster
+        {
+            get
+            {
+                return $"https://image.tmdb.org/t/p/w500/{poster_path}";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using RestSharp;
+using System.Text.Json;
+using Flurl.Http;
+
+namespace APIConsume
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //httpclient
+            //using (var client = new HttpClient())
+            //{
+            //    var request = new HttpRequestMessage(HttpMethod.Get, "https://api.themoviedb.org/3/movie/popular?api_key=837fd3c1b9dffbbe7b69a4a979ab2fe3&language=en-US&page=1");
+            //    var response = client.SendAsync(request).Result;
+            //    string json = response.Content.ReadAsStringAsync().Result;
+
+            //    //var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = false };
+            //    var sonuc = JsonSerializer.Deserialize<MovieList>(json);
+
+            //    foreach (var film in sonuc.results)
+            //    {
+            //        Console.WriteLine(film.title);
+            //    }
+            //}
+
+            //restsharp
+
+            //using (var client = new RestClient())
+            //{
+            //    var request = new RestRequest("https://api.themoviedb.org/3/movie/popular?api_key=837fd3c1b9dffbbe7b69a4a979ab2fe3&language=en-US&page=1", Method.Get);
+            //    var result = client.Get<MovieList>(request);
+
+            //    foreach (var film in result.results)
+            //    {
+            //        Console.WriteLine(film.title);
+            //    }
+            //}
+
+            //flurl
+
+            var sonuc = "https://api.themoviedb.org/3/movie/popular?api_key=837fd3c1b9dffbbe7b69a4a979ab2fe3"
+                .GetJsonAsync<MovieList>().Result;
+
+            foreach (var film in sonuc.results)
+            {
+                Console.WriteLine(film.title);
+            }
+
+            Console.Read();
+        }
+    }
+}
